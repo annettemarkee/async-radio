da0560fc5d012b67c979f3d0677549b733cf233a
Anniegit config --global user.email annettemarkee@gmail.com <annettemarkee@gmail.com>
2026-06-23 10:50:42 -0600

    Create ADC library files and add to cmakelist

---
+++ Libraries/Inc/adc_lib.h
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stm32f0xx_hal.h>
+#include <stm32f0xx_hal_gpio.h>
+
+#ifndef ADC_LIB_H
+#define ADC_LIB_H
+
+#include "stm32f0xx_hal.h"
+
+void InitADC(void);
+
+#endif
---
+++ Libraries/Src/adc_lib.c
@@ -0,0 +1,8 @@
+#include <stdint.h>
+#include <stm32f0xx_hal.h>
+#include <stm32f0xx_hal_gpio.h>
+#include "adc_lib.h"
+
+void ADC_Init(uint16_t pin_bits) {
+    
+}
